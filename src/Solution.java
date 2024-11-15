00a96418a7fbf819c580c416ff440456fd4ca928
chihyutsai <cytsai@seas.upenn.edu>
2024-11-15 16:46:13 -0500

    initial commit

---
+++ src/Solution.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Solution {
+
+	//private String target;
+	private int targetSize;
+	private ArrayList<Character> partialSolution;
+	private int missingChars;
+
+	// instead of a fixed word, should just allocate space for a target word
+	public Solution(String target) {
+		this.target = target;
+		missingChars = target.length();
+		partialSolution = new ArrayList<>(missingChars);
+		for (int i = 0; i < target.length(); i++) {
+			partialSolution.add('_');
+		}
+	}
+
+	// writing a similar function to the above one
+
+	public boolean isSolved() {
+		return missingChars == 0;
+	}
+
+	public void printProgress() {
+		for (char c : partialSolution) {
+			System.out.print(c + " ");
+		}
+		System.out.println();
+	}
+
+	public boolean addGuess(char guess) {
+		boolean guessCorrect = false;
+		for (int i = 0; i < target.length(); i++) {
+			if (target.charAt(i) == guess) {
+				partialSolution.set(i, guess);
+				missingChars--;
+				guessCorrect = true;
+			}
+		}
+		return guessCorrect;
+	}
+
+	public String getTarget() {
+		return target;
+	}
+
+}
