da40dc0baf784ec91bff60c71c3a0e5931a8ce72
chihyutsai <cytsai@seas.upenn.edu>
2024-11-22 08:19:20 -0500

    modify getNewCandidateList method

--- src/EvilHangman.java
+++ src/EvilHangman.java
@@ -8,13 +8,13 @@ public class EvilHangman {
     private HashSet<Character> previousGuesses;
     private TreeSet<Character> incorrectGuesses; // behaves like a hash set, but orders the entries!
     private Scanner inputScanner;
+    private EvilSolution evilSolution;
+    private ArrayList<Character> wordFamily;
 
 
     public EvilHangman() {
-
+        this("engDictionary.txt");
     }
-
-
 
 
     public EvilHangman(String filename) {
@@ -30,32 +30,51 @@ public class EvilHangman {
 
         previousGuesses = new HashSet<>();
         incorrectGuesses = new TreeSet<>();
-        //int randomIndex = new Random().nextInt(wordList.size());
-        //String target = wordList.get(randomIndex);
-
+        int targetWordLength = new Random().nextInt(maxLengthInList(wordList));
         inputScanner = new Scanner(System.in);
-
+        evilSolution  = new EvilSolution(targetWordLength, wordList);
     }
 
 
 
-
-
-
-
-
-
-
-
-
     public void start() {
-        while (!EvilSolution.isSolved()) {
+        while (!evilSolution.gotSolved()) {
+            char thisGuess = askForGuess();
+            evilSolution.placeGuess(thisGuess);
 
         }
+    }
 
 
 
+    // ask user to input a guess
+    private char askForGuess() {
+        while (true) {
 
+            System.out.println("Guess a letter.\n");
+            evilSolution.progress();
+
+            System.out.println("Incorrect guesses:\n" + incorrectGuesses.toString());
+            String input = inputScanner.next();
+            if (input.length() != 1) {
+                System.out.println("Please enter a single character.");
+            } else if (previousGuesses.contains(input.charAt(0))) {
+                System.out.println("You've already guessed that.");
+            } else {
+                return input.charAt(0);
+            }
+        }
+
+    }
+
+
+    // record what's being guessed
+    private void recordGuess(char guess) {
+        previousGuesses.add(guess);
+        boolean isCorrect = evilSolution.placeGuess(guess);
+        if (!isCorrect) {
+            incorrectGuesses.add(guess);
+        }
     }
 
 
@@ -84,9 +103,10 @@ public class EvilHangman {
             if (dictionary.get(i).length() > maxLength) {
                 maxLength = dictionary.get(i).length();
             }
-            return  maxLength;
         }
+        return  maxLength;
     }
+
 
 
 
--- src/EvilSolution.java
+++ src/EvilSolution.java
@@ -1,16 +1,112 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class EvilSolution {
     private int solutionLength;
+    private ArrayList<Character> wordPattern;
+    private ArrayList<String> candidateList;
 
-    public EvilSolution (int solutionLength) {
+
+
+    /*
+    the solution is dynamic, don't have a fixed solution
+    loop through all words in the dictionary (stored in an ArrayList) and if word length equals the length that is passed in the constructor, it is a potential candidate
+    this store all the words that have the solutionLength from the dictionary
+    wordFamily original format --> _ _ _ _ (depends on the passed in length)
+    candidateList is the remaining words in the dictionary that can be solution
+    */
+    public EvilSolution (int solutionLength, ArrayList<String> inputList) {
         this.solutionLength = solutionLength;
+        this.candidateList = new ArrayList<>();
+        for (int i = 0; i < inputList.size(); i++) {
+            if (inputList.get(i).length() == solutionLength) {
+                candidateList.add(inputList.get(i));
+            }
+        }
+        this.wordPattern = new ArrayList<>();
+        for (int i = 0; i < solutionLength; i++) {
+            wordPattern.add('_');
+        }
 
     }
 
 
+    // once we have the guess should split existing words in the candidatePool into word family
+    // should have the key that is wordFamily and value is an ArrayList
+    public ArrayList<String> getNewCandidateList(char guess, ArrayList<String> currentList) {
 
-    public boolean isSolved() {
+        HashMap<ArrayList<Character>, ArrayList<String>> intermediateResult = new HashMap<>();
+        ArrayList<String> possibleCandidateList = new ArrayList<>();
 
+        for (int i = 0; i < currentList.size(); i++ ) {
+            for (int j = 0; j < currentList.get(i).length(); j++) {
+                if (tempCopy.get(j) == '_' && candidateList.get(i).charAt(j) == guess) {
+                    tempCopy.set(j, guess);
+                }
+            }
+            tempCopy.clear();
+            for (int k = 0; k < wordFamily.size(); k++) {
+                tempCopy.add(wordFamily.get(k));
+            }
+            possibleCandidateList.add(candidateList.get(i));
+            intermediateResult.put(tempCopy, possibleCandidateList);
+            System.out.println("intermediarte result");
+            System.out.println(intermediateResult);
+        }
+
+        int maxSize = 0;
+        ArrayList<Character> tempKey = new ArrayList<>();
+        // if we update maxSize, we also have to update put wordFamily
+        for (ArrayList<Character> c : intermediateResult.keySet()) {
+            if (intermediateResult.get(c).size() >= maxSize) {
+                maxSize = intermediateResult.get(c).size();
+                tempKey = c;
+            } else if (intermediateResult.get(c).size() == maxSize) {
+                int currNum = 0;
+                int prevNum = 0;
+                for (int i = 0; i < tempKey.size(); i++) {
+                    if (tempKey.get(i) == '_') {
+                        prevNum += 1;
+                    }
+                }
+                for (int  i = 0; i < c.size(); i++) {
+                    if (c.get(i) == '_') {
+                        currNum += 1;
+                    }
+                }
+                if (currNum > prevNum) {
+                    tempKey = c;
+                }
+            }
+        }
+        wordFamily = tempKey;
+        System.out.println("word fam set at end: " + wordFamily);
+        candidateList = intermediateResult.get(intermediateResult.get(tempKey));
+        System.out.println("candidate list at the end");
+        System.out.println(candidateList);
+        return true;
     }
+//    return false;
+
+
+
+
+    // print out the current progress
+    public void progress() {
+        for (char c : wordFamily) {
+            System.out.print(c + " ");
+        }
+        System.out.println();
+    }
+
+
+
+    // check if the EvilHangman is solved
+    public boolean gotSolved() {
+        return !wordFamily.contains('_');
+    }
+
+
 
 }
-
