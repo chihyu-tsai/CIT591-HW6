a9db380670b195fe8e6de68aca88aed4cb81e215
tiff <hytsai@seas.upenn.edu>
2024-11-24 21:52:01 -0500

    changed to engDictionary.txt

--- src/EvilHangmanRunner.java
+++ src/EvilHangmanRunner.java
@@ -1,6 +1,6 @@
 public class EvilHangmanRunner {
     public static void main(String[] args) {
-        EvilHangman h = new EvilHangman("small.txt");
+        EvilHangman h = new EvilHangman("engDictionary.txt");
         h.start();
     }
 }
